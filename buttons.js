8d98294764a95a145afe1b03487b314c7b9cc255
DonRog <losardor+git@gmail.com>
2017-12-01 16:22:49 +0100

    Infobox change 2

--- buttons.js
+++ buttons.js
@@ -39,13 +39,21 @@ function docInfos() {
 
 	info.addTo(mymap);
 	$('.doc_info').prepend('\
+    Systemic risk map of health care in Austria.<br />\
+    For each primary health care provider (HCP) we study how an assumed inoperability of the HCP affects the remaining health care system.\
+    Each circle represents an HCP.<br/>  \
+    The size of the circle indicates the number of treated patients per quarter.<br/>\
+    The color of the circles indicate for how long health care is inaccessible for patients of the local HCP.<br/>\
 		Mouse-over a primary health-care provider to display information.<br />\
 		Circle size represents the number of patients visited per quarter.<br />\
 		Circle color represents the average number of displacements each<br />\
 		patient must undergo to find a new available doctor.<br />\
-		BLUE: low average displacements.<br />\
-		RED: High average displacements.<br />\
-		Double click to remove health-care provider. Observe the drop in<br />\
+		BLUE: short/low reduction of accessibility <br />\
+		RED: long/severe reduction of accessibilty<br />\
+    <b> Scenario creation: <b/>\
+    Double click on a HCP to create a scenario where this HCP becomes inactive. The impact on the remaining health care system can be observed in  the dynamic graph to the bottom right.<br/> \
+    There time progresses on the x-axis (in days). The y-axis shows the percentage of patients in the given district with immediate access to a HCP, which defines the Resilience Indicator Level (RIL).<br/>\
+    to remove health-care provider. Observe the drop in<br />\
 		the percentage of patients being cared for in bottom right graph.<br />\
 		<b>HEALTH-CARE PROVIDERS IS FIXED AT RANDOM WITHIN SAME LAND</b>');
 }
